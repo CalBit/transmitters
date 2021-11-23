44067529d985af6b7369bc6227e2714d9852c553
CalBit <caleb@bittingers.com>
2021-11-22 16:06:22 -0800

    Fixed some issues and cleaned up the code

--- init.lua
+++ init.lua
@@ -1,5 +1,25 @@
+-- Get mod storage
+local storage = minetest.get_mod_storage()
+
 -- Variables
-local channels = {}
+local channels = minetest.parse_json(storage:get_string("channels")) or {}
+
+-- Functions
+function save_channels()
+  storage:set_string("channels", minetest.write_json(channels))
+end
+
+function gen_formspec(default)
+  if default == nil then default = "" end
+
+  local formspec = {
+    "formspec_version[4]",
+    "size[6,4]",
+    "field[1,1;4,0.5;channel;Channel;" .. default .. "]",
+    "button[1,2.5;2,0.5;submit;Submit]"
+  }
+  return table.concat(formspec, "");
+end
 
 -- Nodes
 minetest.register_node("transmitters:sender", {
@@ -15,12 +35,7 @@ minetest.register_node("transmitters:sender", {
   groups = {cracky = 3},
   after_place_node = function (pos)
     local meta = minetest.get_meta(pos)
-    meta:set_string("formspec",
-      "formspec_version[4]" ..
-      "size[6,4]" ..
-      "field[1,1;4,0.5;channel;Channel;]" ..
-      "button[1,2.5;2,0.5;submit;Submit]"
-    )
+    meta:set_string("formspec", gen_formspec())
   end,
   on_receive_fields = function (pos, formname, fields, player)
     if fields.quit then return end
@@ -31,24 +46,21 @@ minetest.register_node("transmitters:sender", {
     meta:set_string("channel", fields.channel)
 
     -- Reset default value for formspec
-    meta:set_string("formspec",
-      "formspec_version[4]" ..
-      "size[6,4]" ..
-      "field[1,1;4,0.5;channel;Channel;" .. fields.channel .. "]" ..
-      "button[1,2.5;2,0.5;submit;Submit]"
-    )
+    meta:set_string("formspec", gen_formspec(fields.channel))
 
-    minetest.chat_send_player(player:get_player_name(), "Transmitters: Channel has been set.")
+    minetest.chat_send_player(player:get_player_name(), "Transmitters: Channel '" .. fields.channel .. "' has been set.")
   end,
   mesecons = {effector = {
     rules = mesecon.rules.default,
     action_on = function (pos)
       local meta = minetest.get_meta(pos)
-      channels[meta.get_string("channel")] = true
+      channels[meta:get_string("channel")] = true
+      save_channels()
     end,
     action_off = function (pos)
       local meta = minetest.get_meta(pos)
-      channels[meta.get_string("channel")] = false
+      channels[meta:get_string("channel")] = false
+      save_channels()
     end
   }}
 })
@@ -64,6 +76,23 @@ minetest.register_node("transmitters:receiver_off", {
     "default_steel_block.png"
   },
   groups = {cracky = 3},
+  after_place_node = function (pos)
+    local meta = minetest.get_meta(pos)
+    meta:set_string("formspec", gen_formspec())
+  end,
+  on_receive_fields = function (pos, formname, fields, player)
+    if fields.quit then return end
+
+    local meta = minetest.get_meta(pos)
+
+    -- Save channel
+    meta:set_string("channel", fields.channel)
+
+    -- Reset default value for formspec
+    meta:set_string("formspec", gen_formspec(fields.channel))
+
+    minetest.chat_send_player(player:get_player_name(), "Transmitters: Channel '" .. fields.channel .. "' has been set.")
+  end,
   mesecons = {receptor = {
     state = mesecon.state.off,
     rules = mesecon.rules.default
@@ -79,6 +108,29 @@ minetest.register_node("transmitters:receiver_on", {
     "default_steel_block.png",
     "default_steel_block.png"
   },
+  after_place_node = function (pos)
+    local meta = minetest.get_meta(pos)
+    meta:set_string("formspec",
+      "formspec_version[4]" ..
+      "size[6,4]" ..
+      "field[1,1;4,0.5;channel;Channel;]" ..
+      "button[1,2.5;2,0.5;submit;Submit]"
+    )
+  end,
+  groups = {cracky = 3},
+  on_receive_fields = function (pos, formname, fields, player)
+    if fields.quit then return end
+
+    local meta = minetest.get_meta(pos)
+
+    -- Save channel
+    meta:set_string("channel", fields.channel)
+
+    -- Reset default value for formspec
+    meta:set_string("formspec", gen_formspec(fields.channel))
+
+    minetest.chat_send_player(player:get_player_name(), "Transmitters: Channel '" .. fields.channel .. "' has been set.")
+  end,
   mesecons = {receptor = {
     state = mesecon.state.on,
     rules = mesecon.rules.default
